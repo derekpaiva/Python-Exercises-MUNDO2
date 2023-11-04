fbfd1e75551832a45d048b678f287585c5211883
Derek Paiva <142218247+derekpaiva@users.noreply.github.com>
2023-11-04 13:43:57 -0300

    Update exe045.py
    Acrescentei cores

--- exe045.py
+++ exe045.py
@@ -11,6 +11,8 @@ sleep(2)
 
 escolha_pessoa = str(input('Pedra, Papel ou Tesoura? '))
 
+print('\n')
+
 escolha_pc = random.choice(jokenpon) # A função que o computaor "escolhe" um dos elementos
 
 if escolha_pc == escolha_pessoa:
@@ -18,27 +20,27 @@ if escolha_pc == escolha_pessoa:
 
 elif escolha_pc == "Pedra":
     if escolha_pessoa == "Papel":
-        print('Você escolheu {} e eu escolhi {} \nVocê venceu! Parabéns!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;42mVocê venceu!\033[m Parabéns!'.format(escolha_pessoa, escolha_pc))
     elif escolha_pessoa == "Tesoura":
-        print('Você escolheu {} e eu escolhi {} \nVocê perdeu!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;43mVocê perdeu!\033[m'.format(escolha_pessoa, escolha_pc))
     else:
         print('JOGADA INVÁLIDA')
 
 elif escolha_pc == "Papel":
     if escolha_pessoa == "Pedra":
-        print('Você escolheu {} e eu escolhi {} \nVocê perdeu!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;43mVocê perdeu!\033[m'.format(escolha_pessoa, escolha_pc))
     elif escolha_pessoa == "Tesoura":
-        print('Você escolheu {} e eu escolhi {} \nVocê venceu! Parabéns!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;42mVocê venceu!\033[m Parabéns!'.format(escolha_pessoa, escolha_pc))
     else:
         print('JOGADA INVÁLIDA')
 
 elif escolha_pc == "Tesoura":
     if escolha_pessoa == "Pedra":
-        print('Você escolheu {} e eu escolhi {} \nVocê venceu! Parabéns!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;42mVocê venceu!\033[m Parabéns!'.format(escolha_pessoa, escolha_pc))
     elif escolha_pessoa == "Papel":
-        print('Você escolheu {} e eu escolhi {} \nVocê perdeu!'.format(escolha_pessoa, escolha_pc))
+        print('Você escolheu {} e eu escolhi {} \n\033[1;30;43mVocê perdeu!\033[m'.format(escolha_pessoa, escolha_pc))
     else:
         print('JOGADA INVÁLIDA')
-        
+
 else:
     print('JOGADA INVÁLIDA')
